5c362cf0be52afd1784446cc1a903805b69cf743
Marcin Zembura <marcinzembura@gmail.com>
2023-05-22 16:12:45 +0200

    Project initialization

---
+++ src/main/java/com/conferencebackend/ConferenceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.conferencebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/conferencebackend/ConferenceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conferencebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
