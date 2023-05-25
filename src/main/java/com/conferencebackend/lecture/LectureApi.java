b696ec43ce08bc521cb9f4b45a2d904ddffdd9bb
Marcin Zembura <marcinzembura@gmail.com>
2023-05-25 20:18:05 +0200

    update LectureApi

--- src/main/java/com/conferencebackend/lecture/LectureApi.java
+++ src/main/java/com/conferencebackend/lecture/LectureApi.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/lectures")
+@RequestMapping("/api/lectures")
 public class LectureApi {
 
     private final LectureService lectureService;
