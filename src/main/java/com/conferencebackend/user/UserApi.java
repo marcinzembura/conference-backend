f47b6b6e92f6118a8aa3f16c10481063f93780f9
Marcin Zembura <marcinzembura@gmail.com>
2023-05-25 20:16:25 +0200

    update UserApi

--- src/main/java/com/conferencebackend/user/UserApi.java
+++ src/main/java/com/conferencebackend/user/UserApi.java
@@ -1,9 +1,35 @@
 package com.conferencebackend.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class UserApi {
+
+    private final UserService userService;
+
+    public UserApi(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{login}")
+    public ResponseEntity<User> getUserByLogin(@PathVariable String login) {
+        User user = userService.getUserByLogin(login);//add throw
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/{login}")
+    public ResponseEntity<Void> updateUserEmail(@PathVariable String login, @RequestParam String newEmail) {
+        userService.updateUserEmail(login, newEmail);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
 }
