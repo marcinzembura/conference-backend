c362a819b88d5067039ec82397f5e3cc467f3f8e
Marcin Zembura <marcinzembura@gmail.com>
2023-05-25 18:45:52 +0200

    User update

---
+++ src/main/java/com/conferencebackend/user/UserApi.java
@@ -0,0 +1,9 @@
+package com.conferencebackend.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserApi {
+}
