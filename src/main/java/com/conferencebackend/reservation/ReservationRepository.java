5378a9b150e66ffd4c33cfc7f4368c12b85b9de3
Marcin Zembura <marcinzembura@gmail.com>
2023-05-25 20:17:33 +0200

    update ReservationRepository

--- src/main/java/com/conferencebackend/reservation/ReservationRepository.java
+++ src/main/java/com/conferencebackend/reservation/ReservationRepository.java
@@ -1,8 +1,15 @@
 package com.conferencebackend.reservation;
 
+import com.conferencebackend.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    List<Reservation> findAllByUserLogin(User login);
+
+
 }
