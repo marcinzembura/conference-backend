b2add438da90a694fc00a56556bfd9b6bac51662
Marcin Zembura <marcinzembura@gmail.com>
2023-05-29 16:59:24 +0200

    removed LoginAlreadyExistsException

--- src/main/java/com/conferencebackend/exception/LoginAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.conferencebackend.exception;
-
-public class LoginAlreadyExistsException  extends RuntimeException {
-
-    public LoginAlreadyExistsException(String login) {
-        super("This login is already taken: " + login);
-    }
-}
